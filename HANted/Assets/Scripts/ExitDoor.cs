1d5fbb961e622412e88e8cb8ae6eb96cb9f90d9f
Simon <s.peters21@student.han.nl>
2024-10-17 14:11:13 +0200

    Added exit door to complete the level

--- HANted/Assets/Scripts/ExitDoor.cs
+++ HANted/Assets/Scripts/ExitDoor.cs
@@ -1,14 +1,13 @@
 using UnityEngine;
 
-public class Door : MonoBehaviour, IInteractable
+public class ExitDoor : MonoBehaviour, IInteractable
 {
     private bool _isOpen;
 
     private float _closedRotation;
     public float openRotation = 90f;
     private float _openedRotation;
-    [SerializeField]
-    private LockHandler _lockHandler;
+    private GameObject controller;
 
     private void Start()
     {
@@ -18,16 +17,9 @@ public class Door : MonoBehaviour, IInteractable
     
     public void Interact()
     {
-        if (!_isOpen)
+        if (!_isOpen && controller != null)
         {
-            if (_lockHandler.IsOpen())
-            {
-                OpenDoor();
-            }
-        }
-        else
-        {
-            CloseDoor();
+            OpenDoor();
         }
     }
 
@@ -36,12 +28,5 @@ public class Door : MonoBehaviour, IInteractable
         transform.eulerAngles = new Vector3(transform.eulerAngles.x, _openedRotation, transform.eulerAngles.z);
         
         _isOpen = true;
-    }
-
-    private void CloseDoor()
-    {
-        transform.eulerAngles = new Vector3(transform.eulerAngles.x, _closedRotation, transform.eulerAngles.z);
-
-        _isOpen = false;
     }
 }
